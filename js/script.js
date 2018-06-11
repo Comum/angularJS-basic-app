f0c0f0b925348a94787ba0ba38d036e27d5d85fb
Comum <miguel.rib.20@gmail.com>
2018-06-11 23:08:34 +0100

    Small refactoring

--- js/script.js
+++ js/script.js
@@ -3,13 +3,35 @@ var app = angular
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when('/main', {
-                templateUrl: 'main.html',
+                templateUrl: 'templates/main.html',
                 controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'templates/about.html',
+                controller: 'MainCtrl'
+            })
+            .when('/services', {
+                templateUrl: 'templates/services.html',
+                controller: 'ServicesCtrl'
+            })
+            .when('/contact', {
+                templateUrl: 'templates/contact.html',
+                controller: 'ContactCtrl'
             })
             .otherwise({
                 redirectTo: '/main'
             });
     }])
-    .controller('MainCtrl', [function () {
-        console.log('This is the main controller.');
+    .controller('MainCtrl', ['$scope', function ($scope) {
+        console.log('Main controller');
+    }])
+    .controller('ServicesCtrl', ['$scope', '$http', function ($scope, $http) {
+        $http
+            .get('/json/services.json')
+            .then(function (response) {
+                $scope.services = response.data;
+            });
+    }])
+    .controller('ContactCtrl', ['$scope', function ($scope) {
+        console.log('Contact controller');
     }]);
